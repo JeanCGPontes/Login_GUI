59f37f7f269c179d5662723d0659c66116f86a66
jeane <Jean19112001>
2021-11-03 13:33:13 -0300

    update register window

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import functions
 import windows
 
 import PySimpleGUI as sg
 import sqlite3
-
 
 if __name__ == "__main__":
     login_window = windows.login_window()
@@ -26,16 +26,29 @@ if __name__ == "__main__":
 
         if window == register_window and event == 'register_button':
             name, email, password = values['full_name'], values['email'], values['password']
-            database = sqlite3.connect('database_Login_GUI.db')
-            cursor = database.cursor()
-            insert_values = f"""INSERT INTO user_data (name, email, password) VALUES ('{name}', '{email}', '{password}')"""
-            cursor.execute(insert_values)
-            database.commit()
-            database.close()
 
-            windows.popup_success_registered()
-            register_window.hide()
-            login_window.un_hide()
+            if name and email and password != '':
+                database = sqlite3.connect('database_Login_GUI.db')
+                cursor = database.cursor()
+                insert_values = f"""INSERT INTO user_data (name, email, password) VALUES ('{name}', '{email}', '{password}')"""
+                cursor.execute(insert_values)
+                database.commit()
+                database.close()
+
+                windows.popup_success_registered()
+                register_window.hide()
+                login_window.un_hide()
+
+            elif name == '':
+                if email == '':
+                    if password == '':
+                        windows.popup_no_input_data()
+                    else:
+                        windows.popup_incomplete_registration()
+                else:
+                    windows.popup_incomplete_registration()
+            else:
+                windows.popup_incomplete_registration()
 
         if window == register_window and event == 'login_button':
             register_window.hide()
